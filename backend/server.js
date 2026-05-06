576e63f88cfdfa8d1013d508920762564ee043f0
Harsh Saraswat <harshsaraswat09@gmail.com>
2026-05-06 14:57:16 +0530

    docker class 2

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,9 @@
-import express from "express"
+import app from "./src/app.js"
 
-const app = express()
-const port = 3000   
+const port = 3000
 
-app.get('/', (req, res) => {
-  res.status(200).json({message: "Hello World"})
-})
 
-app.get("/api/data", (req, res)=>{
-    const data = {
-        id: 1,
-        name: "Sample Data",
-        description: "this is sample"
-    }
-    res.status(200).json(data)
-})
 
-app.listen(port,'0.0.0.0', ()=>{
+app.listen(port, () => {
     console.log("Server is running on port 3000")
 })
